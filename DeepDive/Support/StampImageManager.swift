b9fe0e84afaf1bfe99850534f10a009815d75e15
Gregory Sinnott <gnsinnott@gmail.com>
2023-07-24 13:00:26 -0600

    StampImageManager

--- DeepDive/Support/StampImageManager.swift
+++ DeepDive/Support/StampImageManager.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import SwiftUI
+import PhotosUI
+
+
+func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+}
+
+func saveStampImage(stamp: UIImage, id: String) {
+    let filename = getDocumentsDirectory().appendingPathComponent(id)
+    if let data = stamp.pngData() {
+            try? data.write(to: filename)
+        }
+}
+
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
